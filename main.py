7606b76263d9047e17ba6addf5425587cb40f1ee
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-05-08 11:03:37 +0300

    Add draft implementation of tesseract

--- main.py
+++ main.py
@@ -11,31 +11,53 @@ def extract_text_from_screenshot(image_path):
 
     # Extract text from image using pytesseract
     text = pytesseract.image_to_string(img)
+    print(f"Extracted {image_path}")
     return text
 
 
 def process_screenshots_folder(folder_path):
     results = {}
+    # Create results directory if it doesn't exist
+    os.makedirs('results', exist_ok=True)
+    folder_images = get_folder_images(folder_path)
+    print("Folder images got")
 
-    # Loop through all folders in the main folder
-    for folder_name in os.listdir(folder_path):
-        print(folder_name)
-        folder_full_path = os.path.join(folder_path, folder_name)
-
-        if os.path.isdir(folder_full_path):
-            results[folder_name] = {}
-
-            # Loop through all files in each folder
-            for filename in os.listdir(folder_full_path):
-                if filename.endswith(('.png', '.jpg', '.jpeg')):
-                    image_path = os.path.join(folder_full_path, filename)
-                    text = extract_text_from_screenshot(image_path)
-                    results[folder_name][filename] = text
+    # Then process images for each folder
+    for folder_name, images in folder_images.items():
+        results[folder_name] = {}
+        folder_results_path = os.path.join('results', folder_name)
+        os.makedirs(folder_results_path, exist_ok=True)
+        for filename, image_path in images:
+            text = extract_text_from_screenshot(image_path)
+            results[folder_name][filename] = text
+            results_file = os.path.join(folder_results_path, f'{filename}.txt')
+            with open(results_file, 'w', encoding='utf-8') as f:
+                f.write(f'Text extracted from {filename}:\n')
+                f.write(f'{text}\n')
 
     return results
 
+
+def get_folder_images(folder_path):
+    folder_images = {}
+    # First collect all image files
+    for folder_name in os.listdir(folder_path):
+        folder_full_path = os.path.join(folder_path, folder_name)
+
+        if os.path.isdir(folder_full_path):
+            images = []
+            for filename in os.listdir(folder_full_path):
+                if filename.endswith(('.png', '.jpg', '.jpeg')):
+                    image_path = os.path.join(folder_full_path, filename)
+                    images.append((filename, image_path))
+            folder_images[folder_name] = images
+
+    return folder_images
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("START")
     screenshots_folder = 'data_screenshots'
     results = process_screenshots_folder(screenshots_folder)
 
