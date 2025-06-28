66a4d7a3c4a6737ecf215420ceea5bf9cec74e12
Arsenii Venherak <arsenii.venherak@gmail.com>
2025-06-28 16:32:47 +0300

    Refactor MoveReconstructor: remove deprecated move reconstruction logic and streamline `reconstruct_moves` method signature for clarity.

--- src/core/service/move_reconstructor.py
+++ src/core/service/move_reconstructor.py
@@ -20,60 +20,6 @@ class Move:
     street: Street
     total_pot_contribution: float  # Total contribution this street
 
-#
-# class MoveReconstructor:
-#
-#     def reconstruct_moves(self, current_game) -> List[Move]:
-#         current_street = current_game.get_street()
-#         if not current_street:
-#             return []
-#
-#         moves = []
-#         current_bids = current_game.current_bids
-#
-#         if not current_bids:
-#             return moves
-#
-#         # Find highest bid (the betting level)
-#         max_bid = max(current_bids.values()) if current_bids else 0.0
-#
-#         for player_num in range(1, 7):
-#             player_bid = current_bids.get(player_num, 0.0)
-#
-#             if player_bid == 0:
-#                 # No bid detected = player folded
-#                 move = Move(
-#                     player_number=player_num,
-#                     action_type=ActionType.FOLD,
-#                     amount=0.0,
-#                     street=current_street,
-#                     total_pot_contribution=0.0
-#                 )
-#                 moves.append(move)
-#             elif player_bid == max_bid and player_bid > 0:
-#                 # Has the highest bid = raised (or initial bet)
-#                 move = Move(
-#                     player_number=player_num,
-#                     action_type=ActionType.RAISE,
-#                     amount=player_bid,
-#                     street=current_street,
-#                     total_pot_contribution=player_bid
-#                 )
-#                 moves.append(move)
-#             else:
-#                 # Has a bid but not the highest = called
-#                 move = Move(
-#                     player_number=player_num,
-#                     action_type=ActionType.CALL,
-#                     amount=player_bid,
-#                     street=current_street,
-#                     total_pot_contribution=player_bid
-#                 )
-#                 moves.append(move)
-#
-#         return moves
-
-
 class MoveReconstructor:
 
     def reconstruct_moves(self, current_bids: Dict[int, float], previous_bids: Dict[int, float],
