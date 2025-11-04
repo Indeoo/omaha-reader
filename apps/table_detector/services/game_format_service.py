d620116a53dbe6d59371b2f76b0dac58c233cf1c
Arsenii Venherak <arsenii.venherak@gmail.com>
2025-11-04 20:10:18 +0200

    Refactor `GameFormatService` to simplify method parameters, remove redundant code, and delegate link generation to `FlopHeroLinkService`.

--- apps/table_detector/services/game_format_service.py
+++ apps/table_detector/services/game_format_service.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 from shared.domain.detection import Detection
 from shared.domain.game_snapshot import GameSnapshot
@@ -9,16 +9,18 @@ class GameFormatService:
 
     @staticmethod
     def game_to_dict(window_name: str, game: GameSnapshot) -> dict:
+        from table_detector.services.flophero_link_service import FlopHeroLinkService
+
         return {
             'window_name': window_name,
             'player_cards_string': format_cards_simple(game.player_cards),
             'table_cards_string': format_cards_simple(game.table_cards),
             'player_cards': GameFormatService._format_cards_for_web(game.player_cards),
             'table_cards': GameFormatService._format_cards_for_web(game.table_cards),
-            'positions': GameFormatService._get_positions_for_web(game),
-            'moves': GameFormatService._get_moves_for_web(game),
+            'positions': GameFormatService._get_positions_for_web(game.positions),
+            'moves': GameFormatService._get_moves_for_web(game.moves),
             'street': game.get_street_display(),
-            'solver_link': GameFormatService._get_solver_link_for_web(game)
+            'solver_link': FlopHeroLinkService.generate_link(game)
         }
 
     @staticmethod
@@ -37,11 +39,11 @@ class GameFormatService:
         return formatted
 
     @staticmethod
-    def _get_moves_for_web(game: GameSnapshot) -> List[Dict]:
+    def _get_moves_for_web(moves) -> List[Dict]:
         moves_by_street = []
 
         # Use Game's domain method to get moves by street
-        for street, moves in game.moves.items():
+        for street, moves in moves.items():
             street_moves = []
             for position, move_type in moves:
                 street_moves.append({
@@ -57,19 +59,12 @@ class GameFormatService:
         return moves_by_street
 
     @staticmethod
-    def _get_solver_link_for_web(game: GameSnapshot) -> Optional[str]:
-        from table_detector.services.flophero_link_service import FlopHeroLinkService
-
-        return FlopHeroLinkService.generate_link(game)
-
-
-    @staticmethod
-    def _get_positions_for_web(game: GameSnapshot) -> List[Dict]:
-        if not game.positions:
+    def _get_positions_for_web(positions) -> List[Dict]:
+        if not positions:
             return []
 
         formatted = []
-        for player_num, position in enumerate(game.positions.values(), 1):
+        for player_num, position in enumerate(positions.values(), 1):
             formatted.append({
                 'player': player_num,
                 'player_label': f'Player {player_num}',
